6bcc57c9f3afea6063ff636bcf7d93139c046b6f
Gus Suarez <gus.suarez@zeeto.io>
2017-04-18 21:41:36 -0700

    adds send onEnter

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/', express.static(path.join(__dirname, '/public')))
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.on('clientMessage', (payload) => {
+    io.emit('serverMessage', payload);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 
