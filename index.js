829819e6be9d5a4094353cb6e3d8b4d26188ad9a
Gus Suarez <gus.suarez@zeeto.io>
2017-04-18 21:15:09 -0700

    user can connect

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.Server(app);
+const io = socketIO(server);
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', express.static(path.join(__dirname, '/public')))
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 
 server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
